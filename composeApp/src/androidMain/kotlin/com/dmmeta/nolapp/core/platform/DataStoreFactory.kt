e4eedd722bd63a63b2a7883e2846b004a93fb221
Ferdous <ferdoushasan785@gmail.com>
2025-09-22 11:38:13 +0600

    feat(ui): DataStore Implementation

---
+++ composeApp/src/androidMain/kotlin/com/dmmeta/nolapp/core/platform/DataStoreFactory.kt
@@ -0,0 +1,17 @@
+package com.dmmeta.nolapp.core.platform
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+actual class DataStoreFactory(private val context: Context) {
+    actual fun createDataStore(): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+}
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "user_preferences"
+)
